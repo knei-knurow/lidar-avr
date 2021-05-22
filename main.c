d07db936e8de1910387076bb3f812f1098a5a333
Bartek Pacia <barpac02@gmail.com>
2021-05-22 15:07:51 +0200

    generate PWM for servo correctly
    Co-authored-by: dsonyy <dsonyyy@gmail.com>

--- main.c
+++ main.c
@@ -1,35 +1,35 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define MIN_DUTY 1600
+#define MAX_DUTY 4400
+
 int main(void) {
   // Set Fast-PWM mode.
-  TCCR2A |= (1 << WGM20);
-  TCCR2A |= (1 << WGM21);
+  TCCR1A |= (1 << WGM11);
+  TCCR1B |= (1 << WGM12);
+  TCCR1B |= (1 << WGM13);
 
   // Set non-inverting mode.
-  TCCR2A |= (1 << COM2A1);
+  TCCR1A |= (1 << COM1A1);
 
   // Set DDRB to be output (we use it for OC2 pin for compare registers later).
-  DDRB |= (1 << PB3);
+  DDRB |= (1 << PB1);
 
   // Set PWM duty.
-  OCR2A = 50;
+  OCR1A = MAX_DUTY;
 
-  // Enable prescaler and set to 1 (no prescaling).
-  TCCR2B |= (1 << CS21);
+  // Set PWM period and prescaler (period = 20ms; prescaler = 8).
+  ICR1 = 39999;
+  TCCR1B |= (1 << CS11);
 
   // DDRB = Data Direction Register for port B.
   // Setting PIN5 on PORTB to 1. 1 means it is an output pin.
 
-  /* Replace with your application code */
   while (1) {
-    for (int i = 0; i <= 255; i++) {
-      _delay_ms(100);
-      OCR2A = i;
-    }
-    for (int i = 0; i <= 255; i++) {
-      _delay_ms(100);
-      OCR2A = 255 - i;
+    for (int i = MIN_DUTY; i <= MAX_DUTY; i++) {
+      _delay_ms(10);
+      OCR1A = i;
     }
   }
 }
