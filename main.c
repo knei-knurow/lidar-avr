8583aea72d16c4e90892ba802864d8960770b94a
Bartek Pacia <barpac02@gmail.com>
2021-05-22 13:41:45 +0200

    SZymon's code

--- main.c
+++ main.c
@@ -2,20 +2,18 @@
 #include <util/delay.h>
 
 int main(void) {
-  DDRB |= (1 << PB5);
-
   // Set Fast-PWM mode.
   TCCR2A |= (1 << WGM20);
   TCCR2A |= (1 << WGM21);
 
-  // Set inverting mode.
+  // Set non-inverting mode.
   TCCR2A |= (1 << COM2A1);
 
   // Set DDRB to be output (we use it for OC2 pin for compare registers later).
   DDRB |= (1 << PB3);
 
   // Set PWM duty.
-  OCR2A = 127;
+  OCR2A = 50;
 
   // Enable prescaler and set to 1 (no prescaling).
   TCCR2B |= (1 << CS21);
@@ -23,9 +21,15 @@ int main(void) {
   // DDRB = Data Direction Register for port B.
   // Setting PIN5 on PORTB to 1. 1 means it is an output pin.
 
+  /* Replace with your application code */
   while (1) {
-    _delay_ms(1000);
-
-    PORTB ^= (1 << PB5);
+    for (int i = 0; i <= 255; i++) {
+      _delay_ms(100);
+      OCR2A = i;
+    }
+    for (int i = 0; i <= 255; i++) {
+      _delay_ms(100);
+      OCR2A = 255 - i;
+    }
   }
 }
